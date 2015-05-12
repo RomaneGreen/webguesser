5e30f604dffdee0a41d5897174d2d1a25be91f39
Delphine Foo-Matkin <delphinef@gmail.com>
2015-05-11 17:51:36 -0700

    Added CSS color change

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,21 +1,32 @@
 require 'sinatra'
 require 'sinatra/reloader'
+
 SECRET_NUMBER = rand(100)
 
 def check_guess(num)
 	if num > SECRET_NUMBER
-		message = "Too high!"
+		if num > SECRET_NUMBER + 5
+			return "Way too high!", "veryWrong"
+		else
+			return "Too high!", "slightlyWrong"
+		end
 	elsif num < SECRET_NUMBER
-		message = "Too low!"
+		if num < SECRET_NUMBER - 5
+			return "Way too low!", "veryWrong"
+		else
+			return "Too low!", "slightlyWrong"
+		end
 	else 
-		message = "You got it right! The SECRET NUMBER is #{SECRET_NUMBER}. "
+		return "You got it right! The SECRET NUMBER is #{SECRET_NUMBER}.", "correct"
 	end
 end
 
 get '/' do 
+
 	guess = params["guess"].to_i 
-	message = check_guess(guess)
-	erb :index, :locals => {:message => message}
+	message = check_guess(guess)[0]
+	body_class = check_guess(guess)[1]
+	erb :index, :locals => {:message => message, :body_class => body_class}
 end
 
 
